84d45ee7b8013ad1bb8df26caced1a585e7755a9
RayenDz <132817343+RayenDz@users.noreply.github.com>
2024-08-29 11:01:34 +0200

    Update process_form.php

--- process_form.php
+++ process_form.php
@@ -1,7 +1,5 @@
 <?php
-// ملف process_form.php
-
-// التحقق من القيم المدخلة
+// بيانات النموذج
 $name = htmlspecialchars($_POST['name']);
 $email = htmlspecialchars($_POST['email']);
 $phone = htmlspecialchars($_POST['phone']);
@@ -9,7 +7,7 @@ $subject = htmlspecialchars($_POST['subject']);
 $message = htmlspecialchars($_POST['message']);
 
 // إعداد البريد الإلكتروني
-$emailTo = 'rekabrayen@gmail.com';
+$emailTo = 'rekabrayen@gmail.com'; // عنوان البريد الإلكتروني الذي سيصلك الإشعار عليه
 $emailSubject = 'تسجيل جديد من موقعك';
 $emailHeaders = "MIME-Version: 1.0" . "\r\n";
 $emailHeaders .= "Content-type:text/html;charset=UTF-8" . "\r\n";
@@ -26,37 +24,10 @@ $emailBody = "<html>
 </body>
 </html>";
 
-// إرسال البريد الإلكتروني والتحقق من نجاح الإرسال
+// إرسال البريد الإلكتروني
 if(mail($emailTo, $emailSubject, $emailBody, $emailHeaders)) {
     echo 'تم إرسال البريد الإلكتروني بنجاح!';
 } else {
     echo 'حدث خطأ أثناء إرسال البريد الإلكتروني.';
 }
-
-// إعداد الرسالة النصية باستخدام Twilio
-require_once 'vendor/autoload.php';
-use Twilio\Rest\Client;
-
-$sid = 'YOUR_TWILIO_SID';
-$token = 'YOUR_TWILIO_AUTH_TOKEN';
-$twilioNumber = 'YOUR_TWILIO_PHONE_NUMBER';
-
-$userMessage = "شكرًا لتسجيلك! سنتواصل معك قريباً.";
-$notificationMessage = "تسجيل جديد: \nالاسم: $name\nالبريد الإلكتروني: $email\nرقم الهاتف: $phone\nالموضوع: $subject\nالرسالة: $message";
-
-$client = new Client($sid, $token);
-
-// إرسال رسالة إلى المستخدم
-$client->messages->create(
-    $phone,
-    ['from' => $twilioNumber, 'body' => $userMessage]
-);
-
-// إرسال رسالة إلى الرقم الذي ترغب به
-$client->messages->create(
-    'YOUR_PHONE_NUMBER',
-    ['from' => $twilioNumber, 'body' => $notificationMessage]
-);
-
-echo 'تم إرسال الرسائل بنجاح!';
 ?>
